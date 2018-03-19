956cb9d485a5fe233821d2c686da070995322373
Alex <alexbarke002@gmail.com>
2018-03-19 18:55:16 +0800

    added mongoose

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://abark002:4275_Cleveland@ds213229.mlab.com:13229/express-tutorial-pug';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
